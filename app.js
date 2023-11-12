5192112c88f465c5faac92d4f039df088e202cb1
Yash Vinod Sawant <146620043+yashVSawant@users.noreply.github.com>
2023-11-12 10:34:02 +0530

    first express code

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app=express();
+
+app.use((req ,res,next)=>{
+    console.log('in the middleware ');
+    next();
+})
+
+app.use((req ,res ,next)=>{
+    res.send('<h1>hello from node.js</h1>');
+})
+
+app.listen(3000);
